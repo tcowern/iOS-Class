077edf0e90e073159fb0a09ceca930c31321b4a3
tcowern <tcowern@yahoo.com>
2017-07-18 17:12:34 -0400

    First Commit

---
+++ iOS-Class/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iOS-Class
+//
+//  Created by Thomas Cowern New on 7/18/17.
+//  Copyright © 2017 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Hello World")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
